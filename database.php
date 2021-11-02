e74350e46325fdde0987703de8786116ee58ad38
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-03 02:25:50 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,14 @@
 <?php
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "b64914f07d5e65";
+$password = "f742c533";
 
-$dbserver = "mysql:host=us-cdbr-east-04.cleardb.com";
-$dbname = "dbname=heroku_5142987c57081aa";
-$dbusername = "b64914f07d5e65";
-$dbpassword = "f742c533";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-try{
-    $pdo = new PDO($dbserver . $dbname, $dbusername, $dbpassword);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-}catch(PDOException $e){
-    echo $e->getMessage();
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-
+echo "Connected successfully";
 ?>
