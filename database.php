fc7eec2a41e178252b3e7436fe96c44b1a6dd66f
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-03 02:36:20 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,15 +1,23 @@
 <?php
 $servername = "us-cdbr-east-04.cleardb.com";
-$dbname = "heroku_5142987c57081aa";
 $username = "b64914f07d5e65";
 $password = "f742c533";
 
 // Create connection
-$conn = new mysqli($servername, $dbname, $username, $password);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "ALTER TABLE tblusers
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6";
+
+if($conn->query($sql) === TRUE) {
+  echo "Table successfully updated";
+} else {
+  echo "Error updating table: " . $conn->error;
+}
 ?>
