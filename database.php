67206066d116ff25a9d0065dc3cceb813e7ff832
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-03 02:11:09 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,9 @@
 
 <?php
 
-//$dbserver = "us-cdbr-east-04.cleardb.com";
-//$dbname = "heroku_5142987c57081aa";
-$dsn = 'mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_5142987c57081aa';
-$dbusername = "b64914f07d5e65";
+$dbserver = "us-cdbr-east-04.cleardb.com";
+$dbname = "heroku_5142987c57081aa";
+$dbusername = "b64914f07d5S";
 $dbpassword = "f742c533";
 
 try{
