6b07b360f70ced5ef22bf42dbc5518ed65eadff1
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-03 02:31:23 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,11 @@
 <?php
 $servername = "us-cdbr-east-04.cleardb.com";
+$dbname = "heroku_5142987c57081aa";
 $username = "b64914f07d5e65";
 $password = "f742c533";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $dbname, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
