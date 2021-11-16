7ee445f0ca6259de35600efb977609ff5d8b7960
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-16 12:02:34 +0800

    Create users.php

---
+++ Reciever/users.php
@@ -0,0 +1,19 @@
+<?php
+    //open connection to mysql db
+    $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
+
+    //fetch table rows from mysql db
+    $sql = "select * from tblusers";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+
+    //close the db connection
+    mysqli_close($connection);
+?>
