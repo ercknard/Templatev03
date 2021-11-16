95624235a87be583085586aedc566ab54fbbb721
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-16 12:47:04 +0800

    Update users.php

--- Reciever/users.php
+++ Reciever/users.php
@@ -3,7 +3,7 @@
     $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
     //fetch table rows from mysql db
-    $sql = "select * from tbl_employee";
+    $sql = "select * from tblusers";
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
     //create an array
