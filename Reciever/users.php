56379f4b02d19be810758106c7d0758f5d39be47
ercknard <79645928+ercknard@users.noreply.github.com>
2021-11-16 12:46:06 +0800

    Update users.php

--- Reciever/users.php
+++ Reciever/users.php
@@ -3,26 +3,21 @@
     $connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b64914f07d5e65","f742c533","heroku_5142987c57081aa") or die("Error " . mysqli_error($connection));
 
     //fetch table rows from mysql db
-    $sql = "select * from tblusers";
+    $sql = "select * from tbl_employee";
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
     //create an array
     $mirmo = array();
-    $post = array();
     while($row =mysqli_fetch_assoc($result))
     {
-        $id=$row['id']; 
-            $username=$row['username'];
-            $contact=$row['contact'];  
-
-            $posts[] = array('id'=> $id, 'username'=> $username, 'contact'=> $contact);
+        $mirmo[] = $row;
     }
-    $mirmo['post'] = $post;
+    echo json_encode($mirmo);
 
-        //write to json file
-        $fp = fopen('regusers.json', 'w');
-        fwrite($fp, json_encode($mirmo));
-        fclose($fp);
+    //write to json file
+    $fp = fopen('users.json', 'w');
+    fwrite($fp, json_encode($mirmo));
+    fclose($fp);
 
     //close the db connection
     mysqli_close($connection);
